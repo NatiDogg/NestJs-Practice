df27ad9dc1a2b6d67413d7a7af62976bf8a110d8
natiDogg <natnael.wondmu@gmail.com>
2026-07-25 06:10:04 -0700

    written my first service for hello module

--- src/hello/hello.controller.ts
+++ src/hello/hello.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller,Get } from '@nestjs/common';
+import { HelloService } from './hello.service';
 
 @Controller('hello')
-export class HelloController {}
+export class HelloController {
+
+       constructor(private readonly helloService:HelloService){}
+        
+        @Get()
+        getHello(){
+            return this.helloService.getHello()
+       }
+}
