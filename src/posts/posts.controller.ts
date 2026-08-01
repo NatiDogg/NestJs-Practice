445437c6a056b271b1bcd51efb381f423c188413
natiDogg <natnael.wondmu@gmail.com>
2026-08-01 05:51:07 -0700

    created a CRUD operation for post module

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,4 +1,35 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller,Delete,Get, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
+import { PostsService } from './posts.service';
+import { Post as PostInterface } from './interface/postInterface';
 
 @Controller('posts')
-export class PostsController {}
+export class PostsController {
+
+      constructor(private readonly postsService:PostsService){}
+
+
+      @Get()
+      findAll(){
+        return this.postsService.findAll()
+      }
+      @Get(':id')
+      findOne(@Param('id',ParseIntPipe) id: number){
+            return this.postsService.findOne(id)
+      }
+
+      @Post()
+      createPost(@Body() postDetails: Omit<PostInterface, 'createdAt'>){
+             return this.postsService.createPost(postDetails)
+      }
+
+      @Patch(':id')
+      updatePost(@Param('id',ParseIntPipe) id: number,@Body() postDetails: Omit<PostInterface, 'createdAt'>){
+          this.postsService.updatePost(id,postDetails)
+      }
+
+      @Delete(':id')
+      deletePost(@Param('id',ParseIntPipe) id: number){
+           return this.postsService.deletePost(id)
+      }
+
+}
--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,4 +1,83 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Post } from './interface/postInterface';
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+
+      private posts: Post[] = [
+           {
+            id: 1,
+            title: "First Post",
+            content: "First Post Content",
+            authorName: "nati",
+            createdAt: new Date
+           },
+           {
+            id: 2,
+            title: "Second Post",
+            content: "Second Post Content",
+            authorName: "yonas",
+            createdAt: new Date
+           },
+           {
+            id: 3,
+            title: "Third Post",
+            content: "Third Post Content",
+            authorName: "abebe",
+            createdAt: new Date
+           }
+      ]
+
+      findAll(): Post[]{
+         return this.posts
+      }
+      findOne(id: number):Post{
+         const post = this.posts.find((post)=> post.id === id)
+         if(!post){
+            throw new NotFoundException(`Post with ID: ${id} is not found`)
+         }
+         return post
+      }
+      createPost(postDetails: Omit<Post, 'createdAt'>):Post[]{
+
+         const newlyCreatedPost:Post = {
+             ...postDetails,
+             createdAt: new Date
+             
+         }
+         this.posts.push(newlyCreatedPost)
+         return this.posts
+         
+
+         
+      }
+      updatePost(id: number, updatePostDetails:Partial<Omit<Post, 'createdAt'>>): Post[]{
+         
+         const postIndex = this.posts.findIndex((post)=> post.id === id);
+         if(postIndex === -1){
+              throw new NotFoundException(`Post with ID: ${id} is not found`)
+         }
+
+         this.posts[postIndex] = {
+             ...this.posts[postIndex],
+             ...updatePostDetails
+         }
+         return this.posts
+
+
+      }
+      deletePost(id: number): {success: boolean, message: string, data: Post[]}{
+         const postIndex = this.posts.findIndex((post)=> post.id === id);
+          if(postIndex === -1){
+             throw new NotFoundException(`Post with ID: ${id} is not found`)
+          }
+          this.posts.splice(1,postIndex)
+          return {
+            success: true,
+            message: 'Post Deleted Successfully',
+            data: this.posts
+          }
+      }
+
+
+}
