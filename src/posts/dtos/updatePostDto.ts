732664e00e076a14a893ee1c796ae2dc0794c600
natiDogg <natnael.wondmu@gmail.com>
2026-08-04 06:44:23 -0700

    created a updatePostDto for post

--- src/posts/dtos/updatePostDto.ts
+++ src/posts/dtos/updatePostDto.ts
@@ -1,3 +1,3 @@
-
-
-export class UpdatePostDto{}
+import { PartialType } from "@nestjs/mapped-types";
+import { CreatePostDto } from "./createPostDto";
+export class UpdatePostDto extends PartialType(CreatePostDto){}
