6e793219979d8f9f365a8c52b54282d9874c1de4
natiDogg <natnael.wondmu@gmail.com>
2026-08-04 08:15:17 -0700

    created a post exist pipe for post

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,39 +1,40 @@
-import { Body, Controller,Delete,Get, HttpCode, HttpStatus, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
 import { PostsService } from './posts.service';
-import { Post as PostInterface } from './interface/postInterface';
+
 import { CreatePostDto } from './dtos/createPostDto';
 import { UpdatePostDto } from './dtos/updatePostDto';
+import { PostExistPipe } from './pipes/postExistPipe';
 
 @Controller('posts')
 export class PostsController {
 
-      constructor(private readonly postsService:PostsService){}
+  constructor(private readonly postsService: PostsService) {}
 
+  @Get()
+  findAll() {
+    return this.postsService.findAll();
+  }
 
-      @Get()
-      findAll(){
-        return this.postsService.findAll()
-      }
-      @Get(':id')
-      @HttpCode(HttpStatus.OK)
-      findOne(@Param('id',ParseIntPipe) id: number){
-            return this.postsService.findOne(id)
-      }
+  @Get(':id')
+  @HttpCode(HttpStatus.OK)
+  findOne(@Param('id', ParseIntPipe, PostExistPipe) id: number) {
+    return this.postsService.findOne(id);
+  }
 
-      @Post()
-      @HttpCode(HttpStatus.CREATED)
-      createPost(@Body() postDetails: CreatePostDto){
-             return this.postsService.createPost(postDetails)
-      }
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  createPost(@Body() postDetails: CreatePostDto) {
+    return this.postsService.createPost(postDetails);
+  }
 
-      @Patch(':id')
-      updatePost(@Param('id',ParseIntPipe) id: number,@Body() postDetails: UpdatePostDto){
-           return this.postsService.updatePost(id,postDetails)
-      }
+  @Patch(':id')
+  updatePost(@Param('id', ParseIntPipe, PostExistPipe) id: number, @Body() postDetails: UpdatePostDto) {
+    return this.postsService.updatePost(id, postDetails);
+  }
 
-      @Delete(':id')
-      deletePost(@Param('id',ParseIntPipe) id: number){
-           return this.postsService.deletePost(id)
-      }
+  @Delete(':id')
+  deletePost(@Param('id', ParseIntPipe, PostExistPipe) id: number) {
+    return this.postsService.deletePost(id);
+  }
 
 }
