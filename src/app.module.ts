70f3b3efcd7c7c77f03c9db5e504f645ee6da9c1
natiDogg <natnael.wondmu@gmail.com>
2026-08-07 03:31:24 -0700

    created prisma module and service for prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,11 +6,12 @@ import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
 import { validate } from './utils/envValidation';
 import { PostsModule } from './posts/posts.module';
+import { PrismaModule } from './prisma/prisma.module';
 //root module
 @Module({
   imports: [
     ConfigModule.forRoot({isGlobal: true, validate}),
-    HelloModule, UserModule, PostsModule],
+    HelloModule, UserModule, PostsModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
