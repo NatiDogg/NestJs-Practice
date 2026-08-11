d86a967e01f9be050a08ec6e3e4047074d78ad8e
natiDogg <natnael.wondmu@gmail.com>
2026-08-11 06:20:41 -0700

    fixed my bcryptservice to handle errors

--- src/utils/bcryptService.ts
+++ src/utils/bcryptService.ts
@@ -1,15 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import bcrypt from 'bcrypt'
 
 @Injectable()
 export class BcryptService{
 
      async hashPassword(password: string){
-        return await bcrypt.hash(password, 10)
+          try {
+               return await bcrypt.hash(password, 10)
+          } catch (error) {
+                throw new InternalServerErrorException("Failed to hash password")
+          }
      }
 
      async matchPassword(password: string, userPassword: string){
-          return await bcrypt.compare(password, userPassword)
+          try {
+             return await bcrypt.compare(password, userPassword)  
+          } catch (error) {
+                throw new InternalServerErrorException("Failed to verify password")
+          }
      }
        
 }
