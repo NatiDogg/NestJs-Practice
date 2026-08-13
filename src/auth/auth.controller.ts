f74f843f585c98f65092ae136a1da278441a0ad2
natiDogg <natnael.wondmu@gmail.com>
2026-08-13 05:10:35 -0700

    fixed my user schema

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Controller,Body, Req } from '@nestjs/common';
+import { Controller,Body, Post,Get } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dto/registerDto';
 import { LoginDto } from './dto/loginDto';
@@ -8,18 +8,20 @@ export class AuthController {
 
         constructor(private readonly authService:AuthService){}
 
-
+        @Post('register')
         async register( @Body() registerDetails:RegisterDto){
              return await this.authService.register(registerDetails)
         }
 
+        @Post("login")
         async login(@Body() loginDetails: LoginDto){
               return await this.authService.login(loginDetails)
         }
-
-        async refresh(){
-
-        }
+         
+        @Post("refresh")
+        async refreshToken(@Body("refreshToken") refreshToken: string){
+              return await this.authService.refreshToken(refreshToken)
+        } 
 
 
 
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,10 +4,11 @@ import { AuthController } from './auth.controller';
 import { UserModule } from 'src/user/user.module';
 import { BcryptService } from 'src/utils/bcryptService';
 import { JwtService } from 'src/utils/jwtService';
-
+import {PassportModule} from '@nestjs/passport'
 @Module({
   providers: [AuthService,BcryptService,JwtService],
   controllers: [AuthController],
-  imports: [UserModule]
+  imports: [UserModule,PassportModule.register({defaultStrategy: 'jwt'})],
+  exports: [AuthService]
 })
 export class AuthModule {}
