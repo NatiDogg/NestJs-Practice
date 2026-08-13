314ed544dd510167fb760c6e7f6969a8406165ab
natiDogg <natnael.wondmu@gmail.com>
2026-08-13 04:16:20 -0700

    compeleted auth controller logic

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Controller,Body } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { RegisterDto } from './dto/registerDto';
+import { LoginDto } from './dto/loginDto';
 
 @Controller('auth')
 export class AuthController {
 
-        constructor(){}
+        constructor(private readonly authService:AuthService){}
+
+
+        async register( @Body() registerDetails:RegisterDto){
+             return await this.authService.register(registerDetails)
+        }
+
+        async login(@Body() loginDetails: LoginDto){
+              return await this.authService.login(loginDetails)
+        }
+
+        async refresh(){
+
+        }
+        
+
+
 }
