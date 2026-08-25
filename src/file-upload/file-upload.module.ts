d4f14f9712ef6a7c59c97b67b366b1cc7d7a5875
natiDogg <natnael.wondmu@gmail.com>
2026-08-25 05:24:00 -0700

    imported the cloudinary module and register multermodule in file upload module

--- src/file-upload/file-upload.module.ts
+++ src/file-upload/file-upload.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { FileUploadController } from './file-upload.controller';
 import { FileUploadService } from './file-upload.service';
+import { CloudinaryModule } from './cloudinary/cloudinary.module';
+import { MulterModule } from '@nestjs/platform-express';
+import { memoryStorage } from 'multer';
 
 @Module({
   controllers: [FileUploadController],
-  providers: [FileUploadService]
+  providers: [FileUploadService],
+  imports:[CloudinaryModule, MulterModule.register({
+    storage: memoryStorage
+  })]
 })
 export class FileUploadModule {}
--- src/file-upload/file-upload.service.ts
+++ src/file-upload/file-upload.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class FileUploadService {}
+export class FileUploadService {
+
+        constructor(private prisma: PrismaService){}
+}
